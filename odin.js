fd2bb168db889b392677992efda6d8b53cd45da0
odinhusky <odinhusky0923@gmail.com>
2020-05-26 15:12:34 +0800

    20200526-1 update

--- odin.js
+++ odin.js
@@ -160,6 +160,20 @@ odin.helper = (function () {
     /**
      * @author odin
      * @class helpers
+     * @description Check if the device is Mobile.
+     * @returns {boolean} true or false
+     */
+    function isMobile() {
+        if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0, 4))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * @author odin
+     * @class helpers
      * @description Convert a value to a string that is actually rendered.
      * @param {any} val The input value
      * @returns {string} string
@@ -256,6 +270,30 @@ odin.helper = (function () {
      */
     function arrLikeToArray2(list) {
         return [].slice.call(list);
+    }
+
+    /**
+     * @author odin
+     * @class helpers
+     * @description Check if the object is in this array
+     * @param arr Array to be checked
+     * @param obj Object 
+     * @returns {boolean} true or false
+     */
+    function include(arr, obj) {
+        return (arr.indexOf(obj) != -1);
+    }
+
+    /**
+     * @author odin
+     * @class helpers
+     * @description Check if the object is not in this array
+     * @param arr Array to be checked
+     * @param obj Object 
+     * @returns {boolean} true or false
+     */
+    function exclude(arr, obj) {
+        return (arr.indexOf(obj) == -1);
     }
 
     /**
@@ -456,6 +494,7 @@ odin.helper = (function () {
         isObject,
         isValidArrayIndex,
         isPromise,
+        isMobile,
         toString,
         toNumber,
         toNumberWithRaix,
@@ -463,6 +502,8 @@ odin.helper = (function () {
         getAllOwnPropertyObj,
         arrLikeToArray1,
         arrLikeToArray2,
+        include,
+        exclude,
         extend,
         arrToObject,
         no,
@@ -1034,11 +1075,110 @@ odin.jq = (function () {
 
     // }
 
+    /**
+     * @author odin
+     * @class jq
+     * @description Go to somewhere     * @param {string} target Anchor ID, required
+     * @param {number} offset y軸偏移值(pixels, e.g. 50 or -50), optional
+     * @param {number} speed 毫秒(1000 == 1秒), optional
+     */
+    function goTo(anchor, offset, speed) {
+
+        var dis,
+            offset = parseFloat(offset) || 0,
+            speed = speed || 400;
+        if (anchor && document.getElementById(anchor) !== null) {
+
+            dis = $('#' + anchor).offset().top + offset;
+
+        } else {
+
+            dis = 0;
+
+        }
+        try {
+
+            $('html, body').animate({
+                scrollTop: parseFloat(dis)
+            }, speed);
+
+        } catch (err) {
+
+            console.log(err.message);
+
+        }
+
+    }
+
+    /**
+     * @author odin
+     * @class jq
+     * @description Go to somewhere     
+     * @param {string||number} disfromTop Could be number or specific word like "top","bottom","down"
+     */
+    function scroll(disfromTop) {
+
+        var dis = disfromTop || 0;
+        if (dis == 'top') {
+
+            dis = 0;
+
+        } else if (dis == 'bottom' || dis == 'down') {
+
+            dis = document.body.scrollHeight;
+
+        }
+        try {
+
+            $('html, body').animate({
+                scrollTop: parseFloat(dis)
+            }, 400);
+
+        } catch (err) {
+
+            console.log(err.message);
+
+        }
+
+    }
+
+    /**
+     * @author odin
+     * @class jq
+     * @description Send a Ajax request of with jQuery object
+     * @param {object} obj 
+     * @example 
+     * {
+     *      url: url,
+     *      dataType: 'json',
+     *      method: 'POST',
+     *      data: {
+     *          ...
+     *      }
+     * }
+     */
+    function sendAjax(obj) {
+        var deferred = $.Deferred();
+
+        $.ajax(obj)
+            .then(function (res) {
+                deferred.resolve(res);
+            })
+            .catch(function (rej) {
+                deferred.resolve(rej);
+            });
+
+        return deferred.promise();
+    }
+
     return {
         isjQeryExist,
         shallowCopy,
         deepCopy,
-        // makeTimeStr
+        // makeTimeStr,
+        goTo,
+        scroll,
+        sendAjax
     };
 
 })();
@@ -1179,10 +1319,236 @@ odin.tools = (function () {
         return resultArr.join('');
     }
 
+    /**
+     * @author odin
+     * @class tools
+     * @description Convert 886 mobile phone to the 09XXXXXXXX
+     * @param {number} num input phone number
+     * @returns {number} Transformed phone number
+     */
+    function mphoneNormalized(num) {
+        num = num.toString();
+        // 去886
+        if (num.length > 5 && num.indexOf('886') === 0) {
+            num = num.substr(3);
+        }
+        // 加0
+        if (num.indexOf('9') === 0) {
+            num = '0' + num;
+        }
+        return num;
+    }
+
+    /**
+     * @author odin
+     * @class tools
+     * @description 全形轉半形程式
+     * @param val string 傳入要轉換的字串 required
+     * @param trimSpace bool => 去除 space, default true
+     * @param trimDash bool => 去除 dash - , default true
+     */
+    function fullToHalf(val, trimSpace, trimDash) {
+
+        var value = val || '',
+            result = '',
+            i;
+
+        for (i = 0; i < value.length; i++) {
+
+            if (value.charCodeAt(i) === 12288) {
+
+                result += ' ';
+
+            } else if (value.charCodeAt(i) > 65280 && value.charCodeAt(i) < 65375) {
+
+                result += String.fromCharCode(value.charCodeAt(i) - 65248);
+
+            } else {
+
+                result += String.fromCharCode(value.charCodeAt(i));
+
+            }
+
+        }
+
+        if (trimSpace || true) result = result.replace(/ /g, '');
+
+        if (trimDash || true) result = result.replace(/-+/g, '');
+
+        return result;
+
+    }
+
+    /**
+     * @author odin
+     * @class tools
+     * @description 
+     * @param {array} attrs - 屬性陣列
+     * @example 
+     *  [
+     *      {
+     *          name: 'aaa',
+     *          value: 'bbb'
+     *      },
+     *      {
+     *          name: 'ccc',
+     *          value: 'ddd'
+     *      },
+     *  ]
+     * @param {Element} ele - 目標DOM元素
+     * @returns {Element} element with attrs
+     */
+    function _setAttrs(ele, attrs) {
+
+        if (Array.isArray(attrs) === false) {
+
+            attrs = [attrs];
+
+        }
+
+        attrs.map(function (attr) {
+
+            ele.setAttribute(attr.name, attr.value);
+
+        });
+
+        return ele;
+
+    }
+
 
     return {
         copyTextToClipboard,
-        convertHTMLXSS
+        convertHTMLXSS,
+        mphoneNormalized,
+        fullToHalf,
+        _setAttrs
+    };
+
+})();
+
+/**
+ * @author odin
+ * @description Tools For web moving
+ */
+
+odin.actions = (function () {
+
+    /**
+     * @author odin
+     * @class actions
+     * @description Go to Top     
+     * @param {number} duration 毫秒(1000 == 1秒), required
+     */
+    function goTop(duration) {
+        // cancel if already on top
+        if (document.scrollingElement.scrollTop === 0) return;
+
+        const totalScrollDistance = document.scrollingElement.scrollTop;
+        let scrollY = totalScrollDistance,
+            oldTimestamp = null;
+
+        function step(newTimestamp) {
+            if (oldTimestamp !== null) {
+                // if duration is 0 scrollY will be -Infinity
+                scrollY -= totalScrollDistance * (newTimestamp - oldTimestamp) / duration;
+                if (scrollY <= 0) return document.scrollingElement.scrollTop = 0;
+                document.scrollingElement.scrollTop = scrollY;
+            }
+            oldTimestamp = newTimestamp;
+            window.requestAnimationFrame(step);
+        }
+        window.requestAnimationFrame(step);
+    }
+
+    return {
+        goTop
+    };
+
+})();
+
+/**
+ * @author odin
+ * @description Tools For localStorage manipulation
+ */
+
+odin.localStorage = (function () {
+
+    /**
+     * @author odin
+     * @class localStorage
+     * @description Handle differet act of localStorage
+     * @param  {string} act - getItem, setItem, removeItem
+     * @param  {string} key
+     * @param  {string} value
+     */
+    function _base(act, key, value) {
+
+        var r = null;
+
+        if (window.localStorage) {
+
+            try {
+
+                r = window.localStorage[act](key, value);
+
+            } catch (e) {
+
+                console.log(e);
+
+            }
+
+        } else {
+
+            console.log('LocalStorage is not support.');
+
+        }
+
+        return r;
+
+    }
+
+    /**
+     * @author odin
+     * @class localStorage
+     * @description Get the value of specific key name from localStorage
+     * @param  {string} key
+     */
+    function get(key) {
+
+        return _base('getItem', key);
+
+    }
+
+    /**
+     * @author odin
+     * @class localStorage
+     * @description Set the value of specific key name to localStorage
+     * @param  {string} key
+     * @param  {string} value
+     */
+    function set(key, value) {
+
+        return _base('setItem', key, value);
+
+    }
+
+    /**
+     * @author odin
+     * @class localStorage
+     * @description Remove the value and key name from localStorage
+     * @param  {string} key
+     */
+    function remove(key) {
+
+        return _base('removeItem', key);
+
+    }
+
+    return {
+        get,
+        set,
+        remove
     };
 
 })();
