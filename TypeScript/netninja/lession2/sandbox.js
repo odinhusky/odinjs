b4879050fa79a0713f2796ca45d82036b518ff4d
Odin Husky <odinhusky0923@gmail.com>
2021-08-04 10:30:19 +0800

    20210804-2 	feat(new comment)

--- TypeScript/netninja/lession2/sandbox.js
+++ TypeScript/netninja/lession2/sandbox.js
@@ -5,3 +5,8 @@ console.log(inputs);
 inputs.forEach(function (input) {
     console.log(input);
 });
+// Compile
+// tsc <要轉換ts檔案> <要被轉換的檔案路徑以及名稱>
+// 如果js與ts的名稱相同，則只需要輸入 => tsc <要轉換ts檔案>
+// Compiling Watch
+// tsc <要轉換ts檔案> <要被轉換的檔案路徑以及名稱> -w
--- TypeScript/netninja/lession2/sandbox.ts
+++ TypeScript/netninja/lession2/sandbox.ts
@@ -9,3 +9,10 @@ console.log(inputs)
 inputs.forEach(input => {
   console.log(input);
 })
+
+// Compile
+// tsc <要轉換ts檔案> <要被轉換的檔案路徑以及名稱>
+// 如果js與ts的名稱相同，則只需要輸入 => tsc <要轉換ts檔案>
+
+// Compiling Watch
+// tsc <要轉換ts檔案> <要被轉換的檔案路徑以及名稱> -w
