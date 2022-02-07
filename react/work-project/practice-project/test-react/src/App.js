73e6f6806c31f38df9f07395183ab3f6c6aed714
Odin Husky <odinhusky0923@gmail.com>
2022-02-07 15:05:17 +0800

    20220207 eslint 自動排版的 test

---
+++ react/work-project/practice-project/test-react/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  console.log('123');
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img alt="logo" className="App-logo" src={logo} />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          a={123333}
+          className="App-link"
+          href="https://reactjs.org"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
