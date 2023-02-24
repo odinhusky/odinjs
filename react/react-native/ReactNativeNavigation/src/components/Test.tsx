b29c10f75f599610afb2f5796bfb671f73793d0a
Odin <odin.chen@letron.tech>
2023-02-24 18:28:41 +0800

    [feat] new ReactNativeNavigation [remove] remove old Project

---
+++ react/react-native/ReactNativeNavigation/src/components/Test.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+export const Test = (): JSX.Element => {
+  return <Text>Odin Text123</Text>;
+};
+
+export default Test;
---
+++ react/react-native/ReactNativeNavigation/src/components/Test2.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+export const Test = (): JSX.Element => {
+  return <Text>Odin Husky</Text>;
+};
+
+export default Test;
